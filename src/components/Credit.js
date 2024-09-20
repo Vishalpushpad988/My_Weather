8de218565ed184ee6bc064651e4d74de4e0f0547
Vishal Pushpad <153808675+Vishalpushpad988@users.noreply.github.com>
2024-09-20 12:25:46 +0530

    Add files via upload

---
+++ src/components/Credit.js
@@ -0,0 +1,14 @@
+import React from "react";
+
+export default function Credit() {
+  return (
+    <div class="credit">
+    <p>
+      Coded by
+      <a href=""
+        > Vishal Pushpad</a
+      >, <a href="">Open sourced on Github.</a> Hosted by Vercel
+    </p>
+  </div>
+  );
+}
